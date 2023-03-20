de8af10b572dc7de71a5c90a5929ce1f63394f35
robng98 <betonicacio10@gmail.com>
2023-03-20 12:07:58 -0300

    Free() fix/Only dyn. allocated parts of DFA freed

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "Recognize.h"
+#include "Recognize.c"
 #include "Operations.h"
 #include "Converter.c"
 #include "Helper.c"
@@ -12,14 +13,14 @@ int main()
 
     // generateDFA(&dfa1, "../a1.txt");
     // generateDFA(&dfa2, "../a2.txt");
-    // generateDFA(&dfa4, "test.txt");
+    generateDFA(&dfa4, "a2.txt");
 
     // productDFA(&dfa1, &dfa2, &dfa3, 'u');
-    //printDFA(&dfa4);
+    printDFA(&dfa4);
     //   finalsToString(dfa3);
-    reconWord("test.txt","palavras.txt","palavras-reconhecidas.txt");
+    // simulateDFA("test.txt","palavras.txt","palavras-reconhecidas.txt");
     // printf("%d",dfa4.states[1].initial);
-    // dfaToDOT("test.dot", &dfa4);
+    dfaToDOT("a2.dot", &dfa4);
     //  dfaToFile(dfa1, "../test-complemente.txt");
 
     // FILE *f = fopen("../test.txt", "r");
@@ -28,7 +29,7 @@ int main()
     // freeDFA(&dfa1);
     // freeDFA(&dfa2);
     // freeDFA(&dfa3);
-    freeDFA(&dfa4);
+    // freeDFA(&dfa4);
 
     return 0;
 }
